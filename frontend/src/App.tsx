c13cbafa2b669df930e8b558306940c34dcf415f
kernel72 <d.zachessov@gmail.com>
2020-05-03 22:03:06 +0300

    Base simple logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
+import React, { useContext, useState } from 'react'
+import { Button, Box, Input } from '@material-ui/core'
 
-function App() {
-  return <Button variant="outlined" color="primary">Hello World</Button>
+import GameSessionContext, {
+  GameSessionContextProvider,
+} from 'src/GameSession/context'
+
+const App: React.FC = () => (
+  <GameSessionContextProvider>
+    <Game />
+  </GameSessionContextProvider>
+)
+
+const Game: React.FC = () => {
+  const { startNewGame, currentGameSession, applyWord } = useContext(
+    GameSessionContext,
+  )
+
+  const [wordToApply, setWordToApply] = useState<string>("")
+  return (
+    <Box display="flex" justifyContent="center" flexDirection="column">
+      <Box>
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => startNewGame()}
+        >
+          New Game
+        </Button>
+      </Box>
+      <Box>
+        <Box>SessionId: {currentGameSession?.sessionId}</Box>
+        <Box>Main Word: {currentGameSession?.mainWord}</Box>
+        <Box>History: {currentGameSession?.history.join(', ')}</Box>
+      </Box>
+      <Box display="flex" >
+        <Input onChange={e => setWordToApply(e.target.value.trim())}/>
+        <Button variant="outlined" color="primary" onClick={() => {
+          applyWord(wordToApply)
+        }}>Apply</Button>
+      </Box>
+    </Box>
+  )
 }
 
 export default App
---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = function (app) {
+  app.use(
+    '/api/v1',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api/v1': '/'
+      }
+    }),
+  )
+}
